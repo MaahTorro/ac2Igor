be23fd03051b4c8c72c9505ddf7303404aa316a0
Marcela Torro <99346289+Maahtoro@users.noreply.github.com>
2022-06-20 10:10:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+class info{
+    constructor(McLancheAlegre, BigLanche, McHen){
+        this.McLancheAlegre = McLancheAlegre;
+        this.BigLanche = BigLanche;
+        this.McHen = McHen
+
+    }
+
+    function document_OnLoad() {
+        McLancheAlegre = new ('teste');
+    }
